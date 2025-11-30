3dec6f52854b6bafebcf0ba430369143f44eadec
Mohd Wasim <wasim.ahmad0661@gmail.com>
2025-11-30 17:01:57 +0530

    Add home route to urlpatterns

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from testapp import views
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('testpaper/',views.testpaper),
 ]
